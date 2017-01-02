9084efa4606fb9a00dfde32bde1e47d6c3e061e9
Walee Ahmed <walee.tausif@gmail.com>
2017-01-02 00:04:20 -0500

    Added navbar scrolling

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,28 @@ import 'materialize-css/dist/js/materialize'
 
 $('.button-collapse').sideNav()
 $('#textarea1').trigger('autoresize')
+
+const scrollToAnchor = aid => {
+  const aTag = $(`${aid}`)
+  $('html,body').animate({scrollTop: aTag.offset().top}, 'slow')
+}
+
+$('#landing-link').click(() => {
+  scrollToAnchor('#landing')
+})
+
+$('#past-projects-link').click(() => {
+  scrollToAnchor('#old-projects')
+})
+
+$('#current-projects-link').click(() => {
+  scrollToAnchor('#current-projects')
+})
+
+$('#about-link').click(event => {
+  scrollToAnchor('#about')
+})
+
+$('#contact-link').click(() => {
+  scrollToAnchor('#contact')
+})
